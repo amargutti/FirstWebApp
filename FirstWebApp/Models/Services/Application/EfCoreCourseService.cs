0681492bce3273892ec9bd10c93e9c4be2a8a1aa
amargutti <a.margutti@doctorshop.it>
2025-10-24 18:03:18 +0200

    Add EfCoreCourseService class with interface implementation
    Introduced the `EfCoreCourseService` class implementing the
    `ICourseService` interface within the `FirstWebApp.Models.Services.Application`
    namespace. Added necessary namespaces: `FirstWebApp.Models.EF_Models`
    and `FirstWebApp.Models.ViewModels`. Included a private readonly
    `dBContext` field initialized via the constructor. Added placeholder
    methods `GetCourseAsync` and `GetCoursesAsync` that currently throw
    `NotImplementedException`.

---
+++ FirstWebApp/Models/Services/Application/EfCoreCourseService.cs
@@ -0,0 +1,23 @@
+﻿using FirstWebApp.Models.EF_Models;
+using FirstWebApp.Models.ViewModels;
+
+namespace FirstWebApp.Models.Services.Application
+{
+    public class EfCoreCourseService : ICourseService
+    {
+        private readonly FirstWebAppDBContext dBContext;
+        public EfCoreCourseService(FirstWebAppDBContext dBContext) { 
+            this.dBContext = dBContext;
+        }
+
+        public Task<CourseDetailViewModel> GetCourseAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<CourseViewModel>> GetCoursesAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
